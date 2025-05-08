d1473711f7acac8dbe800336d4442bc09060ebcd
youmin1017 <youmin.main@gmail.com>
2025-05-08 10:43:40 +0800

    feat(trashy): shell alias

--- youmin/pkgs/linux.nix
+++ youmin/pkgs/linux.nix
@@ -12,4 +12,13 @@
       trashy
     ]
   );
+
+  home.shellAliases = {
+    trashrestore =
+      lib.mkIf (!isDarwin)
+        "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trash restore --match=exact --force";
+    trashempty =
+      lib.mkIf (!isDarwin)
+        "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trash empty --match=exact --force";
+  };
 }
