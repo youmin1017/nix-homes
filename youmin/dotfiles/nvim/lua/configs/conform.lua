7210af4b3de170d9528ae89b1ea8f860a5e21e4b
youmin1017 <youmin.main@gmail.com>
2025-05-23 22:10:35 +0800

    feat: nvim config

--- youmin/dotfiles/nvim/lua/configs/conform.lua
+++ youmin/dotfiles/nvim/lua/configs/conform.lua
@@ -2,12 +2,13 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
   },
-
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
 }
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf, timeout_ms = 500 }
+  end,
+})
 
 return options
---
+++ youmin/dotfiles/nvim/lua/plugins/lsp/tailwindcss.lua
@@ -0,0 +1,4 @@
+vim.lsp.enable "tailwindcss"
+
+---@type NvPluginSpec
+return {}
